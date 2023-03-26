c9c965793e23e3214b9e46ec9d95a5dc99c945f4
Егор Плотников <egor2013.ep@gmail.com>
2023-03-26 21:38:54 +0300

    third commit

--- yii2-basic/controllers/SourceController.php
+++ yii2-basic/controllers/SourceController.php
@@ -6,4 +6,8 @@ use yii\rest\ActiveController;
 class SourceController extends ActiveController
 {
     public $modelClass = 'app\models\Source';
+
+    public function actionShow($id){
+        return $id;
+    }
 }
