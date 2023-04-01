8dd30be7abb6a730d192e3f50751502ab11bb572
voin508 <nosirew.costya2016@yandex.ru>
2023-04-02 00:00:22 +0300

    Table source

---
+++ frontend/src/pages/Sources.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SourceTable from '../components/Table';
+
+function App() {
+  return (
+    <div>
+      <h1>Source List</h1>
+      <SourceTable />
+    </div>
+  );
+}
+
+export default App;
