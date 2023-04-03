1fa874a931375dea705f2f262f9dc9d44732b681
voin508 <nosirew.costya2016@yandex.ru>
2023-04-03 20:31:16 +0300

    table.js in component

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -1,42 +1,46 @@
 import '../css/Table.css'
 import React, { useState, useEffect } from 'react';
 
-function SourceTable() {
-  const [sources, setSources] = useState([]);
+class SourceTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { sources: [] };
+  }
 
-  useEffect(() => {
+  componentDidMount() {
     fetch('http://localhost/DashboardWeb/yii2-basic/web/source/')
       .then(response => response.json())
-      .then(data => setSources(data))
+      .then(data => this.setState({ sources: data }))
       .catch(error => console.log(error));
-  }, []);
+  }
 
-  return (
-    <table class="table">
-      <thead>
-        <tr>
-          
-          <th>Id</th>
-          <th>Name</th>
-          <th>Type</th>
-          <th>БАЛДЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЖ</th>
-        </tr>
-      </thead>
-      <tbody>
-        {sources.map(source => (
-          <tr key={source.intSourceId}>
-            <td>{source.intSourceId}</td>
-            <td>{source.txtSourceName}</td>
-            <td>{source.txtSourceType}</td>
-            <td>
-              <button>Edit</button>
-              <button>Delete</button>
-            </td>
+  render() {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Type</th>
+            <th>БАЛДЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЖ</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-  );
+        </thead>
+        <tbody>
+          {this.state.sources.map(source => (
+            <tr key={source.intSourceId}>
+              <td>{source.intSourceId}</td>
+              <td>{source.txtSourceName}</td>
+              <td>{source.txtSourceType}</td>
+              <td>
+                <button>Edit</button>
+                <button>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
 }
-export default SourceTable;
 
+export default SourceTable;
