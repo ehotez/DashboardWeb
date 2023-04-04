9ffcd98853a508e13a49775d220cd271e1ef0646
ehotez <egor2013.ep@gmail.com>
2023-04-04 18:17:42 +0300

    right-click for source table

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -1,56 +1,93 @@
 import '../css/Table.css'
-import React, { useState, useEffect } from 'react';
+import React, { } from 'react';
 
 class SourceTable extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { sources: [],
+    this.state = {
+      sources: [],
+      menuVisible: false,
+      menuX: 0,
+      menuY: 0,
       isPopupVisible: false,
-      deleteId:0 };
+      deleteId: 0,
+      deleteName: 'bruh'
+    };
+    this.menuRef = React.createRef();
   }
 
-  componentDidMount() { 
-    this.fetchSources(); 
-  } 
+  componentDidMount() {
+    this.fetchSources();
+    document.addEventListener('click', this.handleClickOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.handleClickOutside);
+  }
+
+  handleClickOutside = (event) => {
+    if (this.menuRef.current && !this.menuRef.current.contains(event.target)) {
+      this.setState({
+        menuVisible: false,
+      });
+    }
+  };
 
   fetchSources() {
-    fetch('http://localhost/new/DashboardWeb/yii2-basic/web/source/') 
-      .then(response => response.json()) 
-      .then(data => this.setState({ sources: data })) 
-      .catch(error => console.log(error)); 
+    fetch('http://localhost/DashboardWeb/yii2-basic/web/source/')
+      .then(response => response.json())
+      .then(data => this.setState({ sources: data }))
+      .catch(error => console.log(error));
   }
 
-  handleButtonClick = (id) => {
-    this.setState({ deleteId: id});
+  handleDeleteButtonClick(id) {
+    this.setState({ deleteId: id });
     this.setState({ isPopupVisible: true });
+    // const source = this.state.sources.find((source) => source.intSourceId == this.state.deleteId);
+    // console.log(source); //Попытки вывести название удаляемого сурса - deleteId и intSourceId почему то разные
   }
 
-  handleButtonClickClose = () => {
+  handleRightClick(e) {
+    e.preventDefault();
+    this.setState({ menuVisible: true });
+    this.setState({ menuX: e.clientX });
+    this.setState({ menuY: e.clientY });
+  }
+
+  handleMenuClick() {
+    this.setState({ menuVisible: false });
+  }
+
+  handleButtonClickClose() {
     this.setState({ isPopupVisible: false });
   }
 
-  handleButtonDelete = (id) => {
+  handleButtonDelete(id) {
     fetch(`http://localhost/DashboardWeb/yii2-basic/web/source/delete/?id=${id}`, {
-    method: 'DELETE'
-  })
-  .then(() => {
-    this.fetchSources();
-  })
-  .catch(error => console.log(error));
-  this.setState({ isPopupVisible: false });
+      method: 'DELETE'
+    })
+      .then(() => {
+        this.fetchSources();
+      })
+      .catch(error => console.log(error));
+    this.setState({ isPopupVisible: false });
   }
+
+  handleRightClick = this.handleRightClick.bind(this);
+  handleMenuClick = this.handleMenuClick.bind(this);
 
   render() {
     return (
       <div>
-        {this.state.isPopupVisible && 
-          <div className="popup"> 
-           <label className='text'>Ты уверен, другалёк?</label>
-           
+        {this.state.isPopupVisible &&
+          <div className="popup">
+            <label className='text'>Ты уверен, другалёк?<br/>
+             Удалить {}? </label>
+
             <button className='but1' onClick={this.handleButtonDelete.bind(this, this.state.deleteId)}>Да</button>
-            <button className='but2' onClick={this.handleButtonClickClose}>Нет</button>
-          </div> 
-        } 
+            <button className='but2' onClick={this.handleButtonClickClose.bind(this)}>Нет</button>
+          </div>
+        }
         <table className="table">
           <thead>
             <tr>
@@ -58,20 +95,32 @@ class SourceTable extends React.Component {
               <th>Name</th>
               <th>Type</th>
               <th>Link</th>
-              <th>БАЛДЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЖ</th>
             </tr>
           </thead>
           <tbody>
             {this.state.sources.map(source => (
-              <tr key={source.intSourceId}>
+              <tr onContextMenu={this.handleRightClick} key={source.intSourceId}>
                 <td>{source.intSourceId}</td>
                 <td>{source.txtSourceName}</td>
                 <td>{source.txtSourceType}</td>
                 <td>{source.txtSourceLink}</td>
-                <td>
-                  <button>Edit</button>
-                  <button onClick={this.handleButtonClick.bind(this,source.intSourceId)}>Delete</button>
-                </td>
+                {this.state.menuVisible && (
+                  <div
+                    ref={this.menuRef}
+                    style={{
+                      position: 'fixed',
+                      left: this.state.menuX,
+                      top: this.state.menuY,
+                      backgroundColor: 'white',
+                      //border: '1px solid black',
+                      //padding: '5px',
+                    }}
+                    onClick={this.handleMenuClick}
+                  >
+                    <button>Edit</button>
+                    <button onClick={this.handleDeleteButtonClick.bind(this, source.intSourceId)}>Delete</button>
+                  </div>
+                )}
               </tr>
             ))}
           </tbody>
--- frontend/src/pages/SourcePage.js
+++ frontend/src/pages/SourcePage.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import SourceTable from '../components/Table';
 
-function SourcePage() {
-  return (
-    <div>
-      <h1>Source List</h1>
-      <SourceTable />
-    </div>
-  );
+class SourcePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 style={{
+          margin: '20px',
+        }}>Source List</h1>
+        <SourceTable />
+      </div>
+    );
+  }
 }
 
 export default SourcePage;
