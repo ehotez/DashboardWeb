87af4ee06f0bfdd0f11c9f2e3641ae371583beb1
ehotez <egor2013.ep@gmail.com>
2023-04-06 16:04:11 +0300

    txtUserLogin correction

--- yii2-basic/models/User.php
+++ yii2-basic/models/User.php
@@ -6,7 +6,7 @@ namespace app\models;
  * This is the model class for table "tblUser".
  *
  * @property int $intUserId Индентификатор пользователя
- * @property string $txtIUserLogin Логин пользователя
+ * @property string $txtUserLogin Логин пользователя
  * @property string $txtUserPassword Пароль пользователя
  */
 class User extends \yii\db\ActiveRecord
@@ -25,8 +25,8 @@ class User extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['txtIUserLogin', 'txtUserPassword'], 'required'],
-            [['txtIUserLogin', 'txtUserPassword'], 'string', 'max' => 64],
+            [['txtUserLogin', 'txtUserPassword'], 'required'],
+            [['txtUserLogin', 'txtUserPassword'], 'string', 'max' => 64],
         ];
     }
 
@@ -37,7 +37,7 @@ class User extends \yii\db\ActiveRecord
     {
         return [
             'intUserId' => 'Int User ID',
-            'txtIUserLogin' => 'Txt I User Login',
+            'txtUserLogin' => 'Txt I User Login',
             'txtUserPassword' => 'Txt User Password',
         ];
     }
@@ -49,7 +49,7 @@ class User extends \yii\db\ActiveRecord
 
     public static function findByUsername($login)
     {
-	    return static::findOne(['txtIUserLogin' => $login]);
+	    return static::findOne(['txtUserLogin' => $login]);
     }
 
     public function getId()
