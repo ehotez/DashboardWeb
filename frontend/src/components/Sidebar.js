cf11c72a928b1eddcba80eb8907e5fe4c1ba1587
valerap97 <v.v.polikarpova.12.11.02@gmail.com>
2023-04-08 00:37:21 +0300

    Sidebar update
    Buttons to links
    Navbar attempt

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
 import '../css/Sidebar.css';
-import {Navigate} from 'react-router-dom';
-
+import { Navigate } from 'react-router-dom';
+import { NavLink as Link } from 'react-router-dom';
+import styled from 'styled-components';
+export const NavBtn = styled(Link)`
+font-size: 15px;
+margin: 10px 10px;
+text-align: center;
+text-decoration: none;
+border-radius:5px;
+height:10%;
+width:auto;
+border: none;
+background: grey;
+cursor: pointer;
+  &:hover {
+    background: white;
+    color: black;
+  }
+  &:active, &:focus{
+    background: pink;
+    color: black;
+  }
+  &:visited,&:link{
+    text-decoration: none;
+  }
+`;
 class Sidebar extends Component {
   state = {
     barrierVisible: [true, true, true, true],
@@ -12,36 +36,25 @@ class Sidebar extends Component {
   };
 
   handleMainClick() {
-    this.setState({isMain:true})
+    this.setState({ isMain: true })
   }
 
   handleGridClick() {
-    this.setState({isGrid:true})
+    this.setState({ isGrid: true })
   }
 
   handleSourceClick() {
-    this.setState({isSource:true})
+    this.setState({ isSource: true })
   }
 
   render() {
 
     return (
       <div className="sidebar-container">
-
-        <button className="sidebar-button" onClick={() => this.handleMainClick()}>
-          Главная
-        </button>
-        <button className="sidebar-button" onClick={() => this.handleSourceClick()}>
-          Источники
-        </button>
-        <button className="sidebar-button" onClick={() => this.handleButtonClick(2)}>
-          Сетка
-        </button>
-        <button className="sidebar-button-logout" onClick={() => this.handleButtonClick(3)}>
-          Выйти
-        </button>
-        { this.state.isSource &&(<Navigate to='/sources' replace={true}/>)}
-        { this.state.isMain &&(<Navigate to='/main' replace={true}/>)}
+        <NavBtn to='/main'>Главная</NavBtn>
+        <NavBtn to='/about'>Сетка</NavBtn>
+        <NavBtn to='/sources'>Источники</NavBtn>
+        <NavBtn to='/'>Выйти</NavBtn>
       </div>
     );
   }
