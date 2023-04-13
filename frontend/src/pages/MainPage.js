d2760e4eb00a06a25d0480e7efa7b0f504d5ccb7
ehotez <egor2013.ep@gmail.com>
2023-04-13 20:51:17 +0300

    revert

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -4,8 +4,13 @@ import '../css/Sidebar.css';
 import React from 'react';
 import $ from 'jquery';
 
+
 class MainPage extends React.Component {
   componentDidMount(){
+    if (localStorage.getItem("reload") ==="1"){
+      window.location.reload();
+      localStorage.setItem("reload", '0')
+    }
     $(".main-h").css('background', 'white');
     $(".source-h").css('background', 'none');
     $(".grid-size").css('display', 'block');
