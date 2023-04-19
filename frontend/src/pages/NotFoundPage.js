c412bd3c21d78e736c73b82f801fdfa9c687bb8c
ehotez <egor2013.ep@gmail.com>
2023-04-19 23:51:56 +0300

    not found page

--- frontend/src/pages/NotFoundPage.js
+++ frontend/src/pages/NotFoundPage.js
@@ -1,14 +1,17 @@
 import '../css/App.css';
 import React from 'react'
-import Sidebar from '../components/Sidebar';
-// import halloweenPumpkin from '';
+import { NavBtn } from '../components/Sidebar';
 
 class NotFoundPage extends React.Component {
     render() {
         return (
-            <div style={{ position: 'relative', zIndex: 0, backgroundImage: `url(../halloween-pumpkin.gif)` }}>
-                <Sidebar />
-                <h1 style={{ position: 'relative', zIndex: 15 }}>Запрашиваемая страница не найдена!</h1>
+            <div style={{ height: '100%', width: '100%', backgroundColor: 'white', position: 'absolute', zIndex: 10 }}>
+                <text style={{ left: '36%', top: '35%', font: 'message-box', fontSize: '2.5em', position: 'absolute' }}>
+                    404 PAGE NOT FOUND
+                </text>
+                <NavBtn style={{ position: 'absolute', left: '39%', top: '40%', width: '20vw', color: 'darkblue' }} to='/'>
+                    На главную
+                </NavBtn>
             </div>
         );
     }
