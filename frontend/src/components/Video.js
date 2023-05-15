2ac6904d0fc60598765da2a3cacdbeee862845d7
ehotez <egor2013.ep@gmail.com>
2023-05-15 14:50:20 +0300

    minor fixes video

--- frontend/src/components/Video.js
+++ frontend/src/components/Video.js
@@ -5,27 +5,42 @@ import '../css/Grid.css';
 class Video extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      multiplier: 0,
+    }
     this.playerRef = React.createRef();
     this.name = this.props.name;
     this.rtsp_link = this.props.link.toString();
     this.pid = -1;
     this.id = this.props.id;
+    this.screenW = window.innerWidth - 80;
+    this.screenH = window.innerHeight;
     this.link = `http://localhost/DashboardWeb/frontend/src/video/${this.name}.m3u8`;
   }
 
   componentDidMount() {
-    if (localStorage.getItem((localStorage.getItem("auth_user") + "-" + localStorage.getItem('size') + "-" + this.id + 'v')) === '1') {
-    fetch(`http://localhost/DashboardWeb/yii2-basic/web/source/get-video/?link=${this.rtsp_link}&name=${this.name}`, {
-      method: "POST",
-    })
-      .then((response) => response.json())
-      .then((result) => {
-        console.log(result)
-        this.setState({ pid: result })
-      });
+    if (localStorage.getItem('size') === '3x3') {
+      this.setState({ multiplier: 0.329 })
+    } else if (localStorage.getItem('size') === '2x2') {
+      console.log(localStorage.getItem('size'))
+      this.setState({ multiplier: 0.50 })
+      console.log(this.id)
+    } else if (localStorage.getItem('size') === '2x3' && this.id === 'widget-0') {
+      this.setState({ multiplier: 0.66 })
     }
-    localStorage.setItem((localStorage.getItem("auth_user") + "-" + localStorage.getItem('size') + "-" + this.id + 'v'),'2')
-    console.log(localStorage.getItem((localStorage.getItem("auth_user") + "-" + localStorage.getItem('size') + "-" + this.id + 'v')))
+
+    if (localStorage.getItem((localStorage.getItem("auth_user") + "-" + localStorage.getItem('size') + "-" + this.id + 'v')) === '1') {
+      fetch(`http://localhost/DashboardWeb/yii2-basic/web/source/get-video/?link=${this.rtsp_link}&name=${this.name}`, {
+        method: "POST",
+      })
+        .then((response) => response.json())
+        .then((result) => {
+          console.log(result)
+          this.setState({ pid: result })
+        });
+    }
+    localStorage.setItem((localStorage.getItem("auth_user") + "-" + localStorage.getItem('size') + "-" + this.id + 'v'), '2')
+    //console.log(localStorage.getItem((localStorage.getItem("auth_user") + "-" + localStorage.getItem('size') + "-" + this.id + 'v')))
   }
 
   componentWillUnmount() {
@@ -37,7 +52,7 @@ class Video extends Component {
         //this.setState({ pid: -1 })
         console.log(result);
       });
-      localStorage.setItem((localStorage.getItem("auth_user") + "-" + localStorage.getItem('size') + "-" + this.id + 'v'),'1')
+    localStorage.setItem((localStorage.getItem("auth_user") + "-" + localStorage.getItem('size') + "-" + this.id + 'v'), '1')
     console.log('unmount')
   }
 
@@ -51,8 +66,8 @@ class Video extends Component {
           muted={true}
           autoPlay={true}
           controls={true}
-          width="100%"
-          height="calc(100vh);"
+          width={this.screenW * this.state.multiplier}
+          height={this.screenH * this.state.multiplier}
           hlsConfig={{
             autoStartLoad: true,
             startPosition: -1,
