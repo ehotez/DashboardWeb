ef1ae4b0f88c19752549e3ddfb7fd9e96115e240
KorolLich <ahmat.silasila@yandex.ru>
2023-05-18 15:54:30 +0300

    I don\'t know what is it

--- frontend/src/pages/SourcePage.js
+++ frontend/src/pages/SourcePage.js
@@ -7,23 +7,23 @@ import $ from 'jquery';
 class SourcePage extends React.Component {
   constructor(props) {
     super(props)
-      this.widgets = [
-        { id: '0' },
-        { id: '1' },
-        { id: '2' },
-        { id: '3' },
-        { id: '4' },
-        { id: '5' },
-        { id: '6' },
-        { id: '7' },
-        { id: '8' }]
-        this.sizes = ['3x3','2x3','2x2']
-      }
+    this.widgets = [
+      { id: '0' },
+      { id: '1' },
+      { id: '2' },
+      { id: '3' },
+      { id: '4' },
+      { id: '5' },
+      { id: '6' },
+      { id: '7' },
+      { id: '8' }]
+    this.sizes = ['3x3', '2x3', '2x2']
+  }
   componentDidMount() {
-    for(var i in this.sizes){
-      for(var j in this.widgets){
-        localStorage.setItem(localStorage.getItem('auth_user') + '-' + this.sizes[i] + '-widget-' + j+'v', '1')
-        console.log(localStorage.getItem(localStorage.getItem('auth_user') + '-' + this.sizes[i] + '-widget-' + j+'v'))
+    for (var i in this.sizes) {
+      for (var j in this.widgets) {
+        localStorage.setItem(localStorage.getItem('auth_user') + '-' + this.sizes[i] + '-widget-' + j + 'v', '1')
+        // console.log(localStorage.getItem(localStorage.getItem('auth_user') + '-' + this.sizes[i] + '-widget-' + j+'v'))
       }
     }
     localStorage.setItem("reload", '1')
